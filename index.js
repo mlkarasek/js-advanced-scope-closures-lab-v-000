f6f218d713331d959ea1dc0d83efc6fc295171a2
Monica  Karasek <mlkarasek@gmail.com>
2018-09-20 19:26:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function produceDrivingRange(range) {
+   return function(start,finish) {
+     const distance = Math.abs(parseInt(start) - parseInt(finish))
+     if (distance > range) {
+       return `${distance - range} blocks out of range`
+     }
+     else {
+       return `within range by ${range - distance}`
+     }
+   }
+ }
+
+ function produceTipCalculator(percentage) {
+   return function(bill) {
+     return percentage*bill;
+   }
+ }
+
+ function createDriver() {
+   let driverId = 0
+
+     return class {
+       constructor(name) {
+         this.name = name;
+         this.id = ++driverId
+       }
+     }
+   }
